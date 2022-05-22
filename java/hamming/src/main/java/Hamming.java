7b8dc0b74a2f5114a0e509e080e14859fae59a23
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-05-21 21:39:57 -0400

    complete Java Hamming exercise with ahmeed83's community solution

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,34 +1,66 @@
+// Code is ahmeed83's community solution for this exercise
+// https://exercism.org/tracks/java/exercises/hamming/solutions/ahmeed83
+
 public class Hamming {
 
-    private int hammingDistance;
+    int count = 0;
 
-    Hamming(){
-        hammingDistance = 0;
-    }
+    public Hamming(String leftStrand, String rightStrand) {
 
-    public Hamming(String leftStrand, String rightStrand) throws Exception {
-        int mismatches = 0;
-        try {
-            if (leftStrand.length()!=rightStrand.length()){
-                throw new Exception();
-            } else {
-                for (int i =0; i < leftStrand.length(); i++){
-                    if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
-                        mismatches++;
-    
-                    }
-                    
-                }
-            }
-            
-        } catch (Exception e) {
-            System.out.println("The two strand lengths do not match");
+        validate(leftStrand, rightStrand);
+
+        if (rightStrand.isBlank() && leftStrand.isBlank()) {
+
+            count = 0;
+
+        } else {
+
+            calculate(leftStrand, rightStrand);
+
         }
-        
-        this.hammingDistance = mismatches;
+
     }
 
-    public int getHammingDistance() {
-        return hammingDistance;
+    int getHammingDistance() {
+
+        return count;
+
     }
+
+    private void calculate(final String leftStrand, final String rightStrand) {
+
+        for (int i = 0; i < leftStrand.length(); i++) {
+
+            if (rightStrand.charAt(i) != leftStrand.charAt(i)) {
+
+                count++;
+
+            }
+
+        }
+
+    }
+
+    private void validate(final String leftStrand, final String rightStrand) {
+
+        if (leftStrand.isBlank() && !rightStrand.isBlank()) {
+
+            throw new IllegalArgumentException("left strand must not be empty.");
+
+        }
+
+        if (rightStrand.isBlank() && !leftStrand.isBlank()) {
+
+            throw new IllegalArgumentException("right strand must not be empty.");
+
+        }
+
+        if (leftStrand.length() != rightStrand.length()) {
+
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+
+        }
+
+    }
+
 }
