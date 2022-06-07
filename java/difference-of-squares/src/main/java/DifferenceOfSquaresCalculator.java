359d668b5d729d84b23817524621f46997e42570
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-06-07 09:22:43 -0700

    add completed DIfference of Squares exercise

---
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,24 @@
+class DifferenceOfSquaresCalculator {
+
+    int computeSquareOfSumTo(int input) {
+        int sum = 0;
+        for(int i=1; i<=input; i++){
+           sum+=i; 
+        }
+        sum=sum*sum;
+        return sum;
+    }
+
+    int computeSumOfSquaresTo(int input) {
+        int sum = 0;
+        for(int i=1; i<=input; i++){
+            sum+=(i*i);
+        }
+        return sum;
+    }
+
+    int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+
+}
