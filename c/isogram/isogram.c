d688af61ad2b29f59374d27582ecc2a1e4241aa3
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-07-17 16:11:43 -0700

    Rearrange for loops so that they are more appropriately nested to fulfill the desired functionality Insert printf statements to understand the contents of arrays Insert main method to test code

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,5 +1,6 @@
 #include "isogram.h"
 #include <string.h>
+#include <stdio.h>
 
 bool is_isogram(const char phrase[]) {
     unsigned int ctr = 0;
@@ -14,26 +15,39 @@ bool is_isogram(const char phrase[]) {
         // if ((phrase[ctr]>='a' && phrase[ctr]<='z') || (phrase[ctr]>='A' && phrase[ctr]<='Z')) {
 
         // }
-        for (ctrSearch=0; ctrSearch<27; ctrSearch++){
-            if (phrase[ctr] == capLtrs[ctr]){
-                ctCap[ctr]++;
+        for (ctrSearch=0; ctrSearch<26; ctrSearch++){
+            if (phrase[ctr] == capLtrs[ctrSearch]){
+                ctCap[ctrSearch]++;
                 break;
             }
-            if (phrase[ctr] == lowerLtrs[ctr]){
-                ctLower[ctr]++;
-                break;
-            }
-        }
-        for (ctr=0; ctr<26; ctr++){
-            capLtrs[ctr]=capLtrs[ctr]+ctLower[ctr];
-        }
-        for (ctr=0; ctr<26; ctr++){
-            if (capLtrs[ctr]>1){
-                isIso = 0;
+            if (phrase[ctr] == lowerLtrs[ctrSearch]){
+                ctLower[ctrSearch]++;
                 break;
             }
         }
-    }    
+        
+    }
+    for (ctr=0; ctr<26; ctr++){
+            ctCap[ctr]=ctCap[ctr]+ctLower[ctr];
+        }
+    for (ctr=0; ctr<26; ctr++){
+        if (ctCap[ctr]>1){
+            isIso = 0;
+            break;
+        }
+    }
+        
+    for (ctr=0; ctr<26; ctr++) {
+        printf("\nThe count at index %d is %d", ctr, ctCap[ctr]);
+    }
+    printf("\n\n");
+    for (ctr=0; ctr<26; ctr++) {
+        printf("\nThe char at index %d is %c", ctr, capLtrs[ctr]);
+    }
     return isIso;
+}
+
+main(){
+    is_isogram("Test string");
 }
 
