19c88c1c6f196515c815c740c8a575bba223f4bd
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-07-17 23:29:25 -0700

    Remove comments

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -12,21 +12,6 @@ int compute(const char *lhs, const char *rhs){
             }
         }
     } 
-    // else if(strlen(lhs)>strlen(rhs)){
-    //     for(cnt=0; cnt<strlen(rhs); cnt++){
-    //         if (rhs[cnt]!=lhs[cnt]){
-    //             diff++;
-    //         }
-    //     } 
-    //     diff+=(strlen(lhs)-strlen(rhs)+ 1);
-    // } else {
-    //     for(cnt=0; cnt<strlen(lhs); cnt++){
-    //         if (rhs[cnt]!=lhs[cnt]){
-    //             diff++;
-    //         }
-    //     } 
-    //     diff+=(strlen(rhs)-strlen(lhs)+ 1);
-    // }
     else {
         diff = -1;
     }
