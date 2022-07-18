d34c58b6550df9e57fee4a556c605e8e6d7f2cb8
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-07-17 23:13:04 -0700

    Hamming first commit

---
+++ c/hamming/hamming.c
@@ -0,0 +1,30 @@
+#include "hamming.h"
+#include <string.h>
+
+
+int compute(const char *lhs, const char *rhs){
+    int diff = 0;
+    unsigned int cnt = 0;
+    if (strlen(lhs)==strlen(rhs)){
+        for (cnt=0; cnt<strlen(lhs); cnt++){
+            if (rhs[cnt]==lhs[cnt]){
+                diff++;
+            }
+        }
+    } else if(strlen(lhs)>strlen(rhs)){
+        for(cnt=0; cnt<strlen(rhs); cnt++){
+            if (rhs[cnt]==lhs[cnt]){
+                diff++;
+            }
+        } 
+        diff+=(strlen(lhs)-strlen(rhs)+ 1);
+    } else {
+        for(cnt=0; cnt<strlen(lhs); cnt++){
+            if (rhs[cnt]==lhs[cnt]){
+                diff++;
+            }
+        } 
+        diff+=(strlen(rhs)-strlen(lhs)+ 1);
+    }
+    return diff;
+}
