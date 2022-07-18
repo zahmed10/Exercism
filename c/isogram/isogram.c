5a6bf31394166db339ab490342527390f01e95f6
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-07-17 21:02:41 -0700

    Format document with VS Code

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -2,38 +2,49 @@
 #include <string.h>
 #include <stdio.h>
 
-bool is_isogram(const char phrase[]) {
+bool is_isogram(const char phrase[])
+{
     unsigned int ctr = 0;
-    int ctrSearch=0;
+    int ctrSearch = 0;
     bool isIso = 1;
     char capLtrs[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char lowerLtrs[]="abcdefghijklmnopqrstuvwxyz";
-    int ctCap[26]= {0}; 
-    int ctLower[26]={0};
+    char lowerLtrs[] = "abcdefghijklmnopqrstuvwxyz";
+    int ctCap[26] = {0};
+    int ctLower[26] = {0};
 
-    if (phrase!=NULL){
-        for (ctr=0; ctr<strlen(phrase); ctr++){
-            for (ctrSearch=0; ctrSearch<26; ctrSearch++){
-                if (phrase[ctr] == capLtrs[ctrSearch]){
+    if (phrase != NULL)
+    {
+        for (ctr = 0; ctr < strlen(phrase); ctr++)
+        {
+            for (ctrSearch = 0; ctrSearch < 26; ctrSearch++)
+            {
+                if (phrase[ctr] == capLtrs[ctrSearch])
+                {
                     ctCap[ctrSearch]++;
                     break;
                 }
-                if (phrase[ctr] == lowerLtrs[ctrSearch]){
+                if (phrase[ctr] == lowerLtrs[ctrSearch])
+                {
                     ctLower[ctrSearch]++;
                     break;
                 }
-            } 
+            }
+        }
+        for (ctr = 0; ctr < 26; ctr++)
+        {
+            ctCap[ctr] = ctCap[ctr] + ctLower[ctr];
+        }
+        for (ctr = 0; ctr < 26; ctr++)
+        {
+            if (ctCap[ctr] > 1)
+            {
+                isIso = 0;
+                break;
+            }
+        }
     }
-    for (ctr=0; ctr<26; ctr++){
-            ctCap[ctr]=ctCap[ctr]+ctLower[ctr];
-        }
-    for (ctr=0; ctr<26; ctr++){
-        if (ctCap[ctr]>1){
-            isIso = 0;
-            break;
-        }
-    } 
-    } else {
+    else
+    {
         isIso = 0;
     }
     return isIso;
