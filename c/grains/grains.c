52b20cd3caf9bf08b7511bdaf7e4bc79197f3839
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-07-19 09:33:59 -0700

    Remove comments and main method

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -7,20 +7,9 @@ uint64_t square(uint8_t index) {
     } return ans;
 }
 uint64_t total(void) {
-    // int cnt;
-    // uint64_t power = 64;
-    // if (power == 0){
-    //     return 1;
-    // } else {
-    //     return (square(power) + square(power-1));
-    // }
     uint64_t total = 0;
     for (uint64_t power=0; power<65; power++){
         total+=square(power);
     }
     return total;
 }
-
-// main (){
-//     printf("%" PRIu64, total());
-// }
