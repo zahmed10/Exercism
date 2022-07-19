1b943bcb6c8604c39ee06bfa8a81e1d8c264ac52
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-07-19 13:45:02 -0700

    Use shift operator instead of pow

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,10 +1,16 @@
 #include "grains.h"
 
 uint64_t square(uint8_t index) {
-    uint64_t ans= pow(2, index-1);
-    if (index>=65){
+    // uint64_t ans= pow(2, index-1);
+
+    // Shift operator reference: https://en.cppreference.com/w/c/language/operator_arithmetic#Shift_operators
+    unsigned long long ulong_num = 0x1; 
+    uint64_t ans= ulong_num<<(index-1); 
+    
+    if (index>=65 || index == 0){
         ans= 0;
-    } return ans;
+    } 
+    return ans;
 }
 uint64_t total(void) {
     uint64_t total = 0;
