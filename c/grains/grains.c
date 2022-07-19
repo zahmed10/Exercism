1d0896e7b6ccc919067a5807bc39ef63d621cd44
Zayd Ahmed <zahmed10@brophybroncos.org>
2022-07-19 14:52:50 -0700

    Edit grains.c Use 1ULL literal instead of ulong_num Rewrite total to use shift operator in square function instead of for loop

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -4,18 +4,20 @@ uint64_t square(uint8_t index) {
     // uint64_t ans= pow(2, index-1);
 
     // Shift operator reference: https://en.cppreference.com/w/c/language/operator_arithmetic#Shift_operators
-    unsigned long long ulong_num = 0x1; 
-    uint64_t ans= ulong_num<<(index-1); 
-    
+    // unsigned long long ulong_num = 0x1; 
+
     if (index>=65 || index == 0){
-        ans= 0;
+        return 0;
     } 
+    uint64_t ans= 1ULL<<(index-1); 
     return ans;
 }
 uint64_t total(void) {
-    uint64_t total = 0;
-    for (uint64_t power=0; power<65; power++){
-        total+=square(power);
-    }
-    return total;
+    // uint64_t total = 0;
+    // for (uint64_t power=0; power<65; power++){
+    //     total+=square(power);
+    // }
+    // unsigned long long ulong_num = 0x1; 
+    return (square(65)) - 0x1; // https://www.geeksforgeeks.org/sum-of-the-series-20-21-22-2n/
+    // return total;
 }
